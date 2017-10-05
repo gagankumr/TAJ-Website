64de31e5c4c72c27d5fe0349b3e29dc3efb638fe
Gagan Kumar <imgagankumar@gmail.com>
2017-10-05 17:53:49 -0500

    Website updated 10/5/17
    Added online order button to local webpage
    Update Catering link to new catering website
    Update Jquery error where stuff wasnt being executed corectly 
    Added SVG Eatstreet online order button
    Added more items to Popular dishes sectoin
    Other minor bug fixes

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,15 @@
-$(function(){$(".pop").on("click",function(){$(".imagepreview").attr("src",$(this).find("img").attr("src")),$("#imagemodal").modal("show")})}),$(window).width()<991?$("#social_bottom_footer").addClass("text-center"):$("#social_bottom_footer").removeClass("text-center"),$(window).width()<767?$(".text_center_js").addClass("text-center"):$(".text_center_js").removeClass("text-center"),$(".thumbnail").click(function(){$(".modal-body").empty();var a=$(this).parent("a").attr("title");$(".modal-title").html(a),$($(this).parents("div").html()).appendTo(".modal-body"),$("#myModal").modal({show:!0})}); 
- 
+$(function(){$(".pop").on("click",function(){$(".imagepreview").attr("src",$(this).find("img").attr("src")),$("#imagemodal").modal("show")})}); $(window).width()<991?$("#social_bottom_footer").addClass("text-center"):$("#social_bottom_footer").removeClass("text-center"),$(window).width()<767?$(".text_center_js").addClass("text-center"):$(".text_center_js").removeClass("text-center"),$(".thumbnail").click(function(){$(".modal-body").empty();var a=$(this).parent("a").attr("title");$(".modal-title").html(a),$($(this).parents("div").html()).appendTo(".modal-body"),$("#myModal").modal({show:!0})});
+
+
+// $(function() {
+//  $(".pop").on("click", function() {
+//    $(".imagepreview").attr("src", $(this).find("img").attr("src")), $("#imagemodal").modal("show")
+//  })
+// });
+// $(window).width() < 991 ? $("#social_bottom_footer").addClass("text-center") : $("#social_bottom_footer").removeClass("text-center"), $(window).width() < 767 ? $(".text_center_js").addClass("text-center") : $(".text_center_js").removeClass("text-center"), $(".thumbnail").click(function() {
+//  $(".modal-body").empty();
+//  var a = $(this).parent("a").attr("title");
+//  $(".modal-title").html(a), $($(this).parents("div").html()).appendTo(".modal-body"), $("#myModal").modal({
+//    show: !0
+//  })
+// });
